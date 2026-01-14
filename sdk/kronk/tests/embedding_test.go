a26a0d6d21a4ff49eb185037ac721829e651b22f
William Kennedy <bill@ardanlabs.com>
2026-01-14 13:14:14 -0500

    adding NSeqMax to tests

--- sdk/kronk/tests/embedding_test.go
+++ sdk/kronk/tests/embedding_test.go
@@ -34,6 +34,7 @@ func testEmbedding(t *testing.T, mp models.Path) {
 		CacheTypeK:     model.GGMLTypeQ8_0,
 		CacheTypeV:     model.GGMLTypeQ8_0,
 		FlashAttention: model.FlashAttentionEnabled,
+		NSeqMax:        2,
 	})
 
 	if err != nil {
