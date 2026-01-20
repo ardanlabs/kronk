8cfd51d4960a957342b6799cf64e980d0eef285d
Ramon Reichert <ramonreichert.rr@gmail.com>
2026-01-19 23:57:32 -0300

    fix[windows]: fix all windows incompatiblities found

--- cmd/kronk/server/start/start.go
+++ cmd/kronk/server/start/start.go
@@ -26,14 +26,12 @@ func runLocal(cmd *cobra.Command) error {
 
 		logFile, _ := os.Create(logFilePath())
 
-		proc := exec.Command(exePath, "server")
+		proc := exec.Command(exePath, "server", "start")
 		proc.Stdout = logFile
 		proc.Stderr = logFile
 		proc.Stdin = nil
 		proc.Env = append(os.Environ(), envVars...)
-		//	proc.SysProcAttr = &syscall.SysProcAttr{
-		//		Setsid: true,
-		//	}
+		setDetachAttrs(proc)
 
 		if err := proc.Start(); err != nil {
 			return fmt.Errorf("start: %w", err)
