688397261a04cebaba0b608de89d882654e74dec
William Kennedy <bill@ardanlabs.com>
2026-02-05 09:34:08 -0500

    Release 1.17.4
    ## What's Changed:
    
    * Enhanced download logic for projection files
    * New yzma support for parallel batch process of mtmd models
    * Major documentation updates
    * VRAM calculation changes for mtmd models
    * Upgraded dependencies

--- sdk/kronk/kronk.go
+++ sdk/kronk/kronk.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Version contains the current version of the kronk package.
-const Version = "1.17.3"
+const Version = "1.17.4"
 
 // =============================================================================
 
