6455c2a26dde9c342e7ac4a2d19f5eae1fbb6d38
William Kennedy <bill@ardanlabs.com>
2026-02-09 15:15:21 -0500

    Release 1.18.1
    ## What's Changed:
    * Support for Ministral
    * Better defaults for kv cache type

--- sdk/kronk/kronk.go
+++ sdk/kronk/kronk.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Version contains the current version of the kronk package.
-const Version = "1.18.0"
+const Version = "1.18.1"
 
 // =============================================================================
 
