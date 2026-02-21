c7f6a67a37f971bf936f273e8ac895649ced7030
William Kennedy <bill@ardanlabs.com>
2026-02-21 13:55:54 -0500

    Release 1.19.4

--- sdk/kronk/kronk.go
+++ sdk/kronk/kronk.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Version contains the current version of the kronk package.
-const Version = "1.19.3"
+const Version = "1.19.4"
 
 // =============================================================================
 
