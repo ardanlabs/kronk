3d6c420413c5c5d3ac6e16a7f92574acf021e2e8
William Kennedy <bill@ardanlabs.com>
2026-04-15 21:36:51 -0400

    Release 1.22.3
    ## What's Changed
    Introduced a new Jinja module (using gonja from Ardan) and fixed mtmd bug.

--- sdk/kronk/kronk.go
+++ sdk/kronk/kronk.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Version contains the current version of the kronk package.
-const Version = "1.22.2"
+const Version = "1.22.3"
 
 // =============================================================================
 
