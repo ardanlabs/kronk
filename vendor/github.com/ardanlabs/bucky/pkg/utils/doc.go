0501e4ce68af1acd497cea67c5fd58d69af5e63d
leogoesger <leoq91@gmail.com>
2026-07-15 12:44:44 -0700

    fix

--- vendor/github.com/ardanlabs/bucky/pkg/utils/doc.go
+++
@@ -1,4 +0,0 @@
-// Package utils provides small string conversion helpers used by the bucky
-// FFI bindings. It mirrors yzma's pkg/utils so callers do not need to depend
-// on golang.org/x/sys directly.
-package utils
