7eb28732da06fe0de7d188750093640b2c458cf0
William Kennedy <bill@ardanlabs.com>
2026-08-04 23:16:53 -0700

    using latest libs

--- sdk/tools/libs/libs.go
+++ sdk/tools/libs/libs.go
@@ -26,7 +26,7 @@ const (
 
 	// defaultVersion is the well-known working version of llama.cpp used
 	// when no explicit version is provided and AllowUpgrade is false.
-	defaultVersion = "b10254"
+	defaultVersion = "b10276"
 )
 
 // ErrReadOnly is returned by mutating operations on a Libs instance whose
