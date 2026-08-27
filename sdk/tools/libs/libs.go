60c1ed0ec47039840c8d6d956b8e052eb23b0dda
William Kennedy <bill@ardanlabs.com>
2026-08-27 10:34:45 -0400

    small account bug with vram calculator

--- sdk/tools/libs/libs.go
+++ sdk/tools/libs/libs.go
@@ -26,7 +26,7 @@ const (
 
 	// defaultVersion is the well-known working version of llama.cpp used
 	// when no explicit version is provided and AllowUpgrade is false.
-	defaultVersion = "b10642"
+	defaultVersion = "b10645"
 )
 
 // ErrReadOnly is returned by mutating operations on a Libs instance whose
