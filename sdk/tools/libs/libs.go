c7e6cbe8ea6e148120662af2ef2d4585c4a07ae4
William Kennedy <bill@ardanlabs.com>
2026-09-01 22:07:52 -0300

    upgrade llama to b10751 (#918)

--- sdk/tools/libs/libs.go
+++ sdk/tools/libs/libs.go
@@ -27,7 +27,7 @@ const (
 
 	// defaultVersion is the well-known working version of llama.cpp used
 	// when no explicit version is provided and AllowUpgrade is false.
-	defaultVersion = "b10731"
+	defaultVersion = "b10751"
 )
 
 // ErrReadOnly is returned by mutating operations on a Libs instance whose
